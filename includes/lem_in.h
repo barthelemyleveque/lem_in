6c6a7fcab21227d05f74f4d4b30828469a811eaf
Barthelemy LEVEQUE <bleveque@e2r10p1.42.fr>
2019-04-24 15:43:28 +0200

    point h

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -6,7 +6,7 @@
 /*   By: bleveque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/22 11:00:20 by bleveque          #+#    #+#             */
-/*   Updated: 2019/04/24 15:40:15 by bleveque         ###   ########.fr       */
+/*   Updated: 2019/04/24 15:42:54 by bleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ typedef struct		s_link
 {
 	int				flow;
 	int				closed;
-	//struct s_link	*opposite;
+	struct s_link	*opposite;
 	struct s_node	*child;
 	struct s_link	*next;
 }					t_link;
